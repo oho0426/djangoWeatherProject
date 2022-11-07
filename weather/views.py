5db75d5f0e212a52dd6291020f33a480e3a6fce9
yangkai <2366733570@qq.com>
2022-11-07 16:18:26 +0800

    增加了温湿度\日出日落等图标,增加了根据空气质量更新底色功能

--- weather/views.py
+++ weather/views.py
@@ -7,18 +7,21 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     """首页"""
-    city = {"city": "武汉"}
-    res = apiInfo(city)
+    city = {"city": "北京"}
+    total_data = apiInfo(city)
     # 取出今天的天气数据
     today_date = datetime.today().strftime("%Y-%m-%d")
     today_m_d = "%s月%s日" % (datetime.today().month, datetime.today().day)
     # 转换农历日期
+
     zh_date = zhdate.ZhDate.from_datetime(datetime.today())
     today_data = []
-    for x in res['data']:
+    for x in total_data['data']:
         if x['date'] == today_date:
             today_data = x
 
+    # 天气图标路径
+    wea_img_path = 'img/%s.png' % today_data['wea_img']
     return render(request, 'index.html', locals())
 
 
