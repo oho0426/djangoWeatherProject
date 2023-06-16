7574f05483e6675ac0a67fb63d2eda966688ab5f
xb <2366733570@qq.com>
2023-06-16 13:52:03 +0800

    xxx

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import time
 from datetime import datetime
 import zhdate
 import requests
+from functools import reduce
 # today_date = datetime.today().strftime("%Y-%m-%d")
 # url = 'https://v0.yiketianqi.com/api?unescape=1&version=v91&appid=43656176&appsecret=I42og6Lm&ext=&cityid='
 # city = '深圳'
@@ -22,8 +23,13 @@ import requests
 # res = requests.post(url, files={"form_field_name": file})
 # print(res.status_code)
 
-res = {'code': '0', 'data': None, 'msg': 'success'}
-res['data'] = 'test'
-print(json.dumps(res))
-# res['data'] = "provinceJson"
-print(res)
+# res = {'code': '0', 'data': None, 'msg': 'success'}
+# res['data'] = 'test'
+# print(json.dumps(res))
+# # res['data'] = "provinceJson"
+# print(res)
+def sum(a, b):
+    return a + b
+d = 0
+c = 'hello{:^20}'.format('bb')
+print(c)
